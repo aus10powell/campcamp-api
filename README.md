338f1045b624ace9640963a799160ccd88aa676f
blaan <blaan@protonmail.com>
2017-11-10 18:47:45 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CampyCamp api
+Flask app for serving campy data.
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import json
+
+app = Flask(__name__)
+
+@app.route('/api/v1/campsites')
+def index():
+    data = get_campsites()
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+def get_campsites():
+    data = [{
+        'latitude': '39.2030556',
+        'longitude': '-123.1863889',
+        'facilityName': 'CHEKAKA RECREATION AREA LAKE MENDOCINO'
+        },
+        {
+        'latitude': '37.9844444',
+        'longitude': '-120.8447222',
+        'facilityName': 'COYOTE POINT'
+        }]
+    return data
+
+if __name__ == '__main__':
+    app.run()
